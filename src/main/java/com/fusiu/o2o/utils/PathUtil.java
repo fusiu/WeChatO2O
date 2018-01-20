e92a58e7d39d465b2d1a3283af7b39e6efd7b9c0
fusiu <fusiu_online@163.com>
2018-01-20 17:18:54 +0800

    PathUtil 工具类创建 该工具类有两个方法 一、获取当前项目图片根目录 二、根据 shopId 获取子目录

---
+++ src/main/java/com/fusiu/o2o/utils/PathUtil.java
@@ -0,0 +1,40 @@
+package com.fusiu.o2o.utils;
+
+public class PathUtil {
+    /**
+     * 不同的操作系统的文件分隔符不同，路径的书写格式也不同
+     * 因此需要获取操作系统的文件分隔符 swparator
+     */
+    private static String separator = System.getProperty("file.separator");
+
+
+    /**
+     * 获取项目图片的跟路径
+     * @return basePath
+     */
+    public static String getImgBasePath() {
+        /**
+         * 获取操作系统的类型
+         */
+        String os = System.getProperty("os.name");
+        String basePath = "";
+        if (os.toLowerCase().startsWith("win")) {
+            basePath = "D:/projectdev/image/";
+
+        } else {
+            basePath = "/home/fusiu/WeChatO2O/image/";
+
+        }
+        return basePath.replace("/",separator);
+    }
+
+    /**
+     * 根据业务需求返回相对应的子路径
+     * @param shopId 店铺 ID 用于生成子目录
+     * @return imagePath 根据 shopId 创建的子目录
+     */
+    public static String getShopImagePath(Long shopId){
+        String imagePath = String.format("/upload/item/shop/%s/",shopId);
+        return imagePath.replace("/",separator);
+    }
+}
